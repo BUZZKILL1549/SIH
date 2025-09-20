7ef3d1306249233ce705ad9c181de98f88c61d09
BUZZKILL1549 <aushvs10@hotmail.com>
2025-09-20 13:22:24 +0530

    Update Map URL again (2)

--- app.py
+++ app.py
@@ -144,7 +144,7 @@ with tab1:
 with tab2:
     st.header("Smart Bin Map")
 
-    API_URL = "https://sih-h85i.onrender.co/garbage_alert"
+    API_URL = "https://sih-h85i.onrender.co/bins"
 
     try:
         bins = requests.get(API_URL).json()
